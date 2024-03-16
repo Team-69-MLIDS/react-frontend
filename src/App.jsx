e9aa22003bf30ff0494c9c63d6a9d78158e484d1
Salem Nassar <salemnassar5@gmail.com>
2024-03-16 17:48:07 -0500

    added mock data to test

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,57 @@ import "./App.css";
 import Dropdown from "./components/Dropdown";
 
 function App() {
-    const options = [
-        { value: "chocolate", label: "Chocolate" },
-        { value: "strawberry", label: "Strawberry" },
-        { value: "vanilla", label: "Vanilla" },
-    ];
+
+    const algorithms = {
+        algorithms: [
+            {
+                label: "Algorithm 1",
+            },
+            {
+                label: "Algorithm 2",
+            },
+            {
+                label: "Algorithm 3",
+            },
+        ],
+    };
+
+    const datasets = {
+        datasets: [
+            {
+                label: "Dataset 1",
+            },
+            {
+                label: "Dataset 2",
+            },
+            {
+                label: "Dataset 3",
+            },
+        ],
+    };
+
+    const prevRuns = {
+        prevRuns: [
+            {
+                id: 1,
+                label: "Run 1",
+            },
+            {
+                id: 2,
+                label: "Run 2",
+            },
+            {
+                id: 3,
+                label: "Run 3",
+            },
+        ],
+    };
 
     return (
         <div className='test'>
-            <Dropdown options={options} />
+            <Dropdown options={algorithms.algorithms} />
+            <Dropdown options={prevRuns.prevRuns} />
+            <Dropdown options={datasets.datasets} />
         </div>
     );
 }
